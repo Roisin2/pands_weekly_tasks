859156deb9813b92274d7d9b1d4d50ef4cc4ef66
Roisin Stanley <roisinstanley@yahoo.co.uk>
2024-02-19 20:34:55 +0000

    adding account task.

---
+++ accounts.py
@@ -0,0 +1,8 @@
+# Write a python program called accounts.py that reads in a 10 character account number 
+# and outputs the account number with only the last 4 digits showing ...
+# (and the first 6 digits replaced with Xs).
+# Please enter an 10 digit account number: 1234567890
+# (XXXXXX7890)
+# Extra:
+# Modify the program to deal with account numbers of any length ...
+# (yes that is a vague requirement, comment your assumptions)
