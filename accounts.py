bafc7da20684ac7864f7260d6b324928d64ea23d
Roisin Stanley <roisinstanley@yahoo.co.uk>
2024-02-23 15:36:25 +0000

    ammending presentation.

--- accounts.py
+++ accounts.py
@@ -1,8 +1,24 @@
 # Write a python program called accounts.py that reads in a 10 character account number 
 # and outputs the account number with only the last 4 digits showing ...
-# (and the first 6 digits replaced with Xs).
+
+# (and the first 6 digits replaced with Xs). created a new variable called hidden_num ,
+#first 6 charachters of hidden_num are set to x and remaining numbers are the original
+# *** hidden_num = "X" * 6 + account_num [6:]
+# *** return hidden_num
+# reference()
+
+
+
 # Please enter an 10 digit account number: 1234567890
 # (XXXXXX7890)
+# check there is 10 numbers
+# if len(account_number) ! = 10 # return "incorrect number, please enter 10 digit account number"
+#(reference)
+
+#https://stackoverflow.com/questions/4511257/mask-account-number-with-string-formatS
 # Extra:
 # Modify the program to deal with account numbers of any length ...
 # (yes that is a vague requirement, comment your assumptions)
+#stackoverflow.com/questions/17550024/masking-a-number-by-showing-last-x-digits-with-1-mask-character-in-front?rq=3
+ 
+ 
--- bank.py
+++ bank.py
@@ -1,20 +1,15 @@
 # read in the amounts in cents
-
 amount1 = 65
 amount2 = 180
 
 # change to Euro (100c in a euro)
-
 e1 = amount1 / 100
 e2 = amount2 / 100
 
-
 # add the two amounts
-
 total_money = e1 + e2
 
 # print the amounts with decimal point and only two numbers after the point. 
-
 print (f"the sum of the amounts is €{total_money:.2f}")
 
 # referrence https://pythonguides.com/python-print-2-decimal-places/
