29ed5c24078e65b2fdd2481f7b7d1e65b527a042
Roisin Stanley <roisinstanley@yahoo.co.uk>
2024-02-24 12:15:18 +0000

    working progress.

--- accounts.py
+++ accounts.py
@@ -1,25 +1,28 @@
-# Write a python program called accounts.py that reads in a 10 character account number 
-# and outputs the account number with only the last 4 digits showing ...
-# (and the first 6 digits replaced with Xs). 
+# A python program called accounts.py that reads in a 10 character account number. 
+# And outputs the account number with the last 4 digits showing and primary 6 digits replaced with X.
+ 
+#Author: Roisin Stanley
+    #prompt for entering number
+account_1 = input("please enter 10 digit code: ")
+    #check for 10 didgit code
+if len(account_1)!= 10 or not account_1 .isdigit():
+    print("invalid account number")
+else:  #if 10 digits ok only show last 4 numbers
+    masked_number = "xxxxxx" + account_1[-4: ]
+    print("your msked a/c number is:", masked_number)
 
-#created a new variable called hidden_num ,
-#first 6 charachters of hidden_num are set to x and remaining numbers are the original
-#hidden_num = "X" * 6 + account_num [6:]
-#print/return (hidden_num)
-# reference()
 
 
 
-# Please enter an 10 digit account number: 1234567890
-# (XXXXXX7890)
-# check there is 10 numbers
- #if len(account_number) :! = 10 # return "incorrect number, please enter 10 digit account number"
 #(reference)
-
 #https://stackoverflow.com/questions/4511257/mask-account-number-with-string-formatS
 # Extra:
 # Modify the program to deal with account numbers of any length ...
-# (yes that is a vague requirement, comment your assumptions)
+   # if not account_1.isdigit():
+   #     print("invalid account number")
+    #else:
+        #masked_number ="x"* (len(account_1)-4) + account_1[-4: ]
+        #print("your masked a/c numberis:",masked_number)
 #stackoverflow.com/questions/17550024/masking-a-number-by-showing-last-x-digits-with-1-mask-character-in-front?rq=3
  
  
